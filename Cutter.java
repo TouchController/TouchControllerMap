4f9d41eea25de2e88befb2ca613d021b355d2099
fifth_light <liu-yu-jie@foxmail.com>
2025-02-16 21:34:22 +0800

    修复切图脚本

--- Cutter.java
+++ Cutter.java
@@ -1,6 +1,8 @@
-import java.io.*;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Logger;
 
 public class Cutter {
     private static boolean readPixel(BufferedImage image, int x, int y) {
@@ -17,70 +19,60 @@ public class Cutter {
         }
     }
 
+    private static void writeFile(BufferedImage image, File outputDir, String name, int x, int y, int w, int h) throws IOException {
+        if (w <= 0 || h <= 0) {
+            return;
+        }
+        var leftTop = image.getSubimage(x, y, w, h);
+        ImageIO.write(leftTop, "png", new File(outputDir, name));
+    }
+
+    public static void cut(File file, Logger logger) throws IOException {
+        var image = ImageIO.read(file);
+        if (image == null) {
+            logger.severe("Bad file: " + file);
+            return;
+        }
+        logger.info("Process image: " + file);
+
+        var leftStart = 0;
+        while (!readPixel(image, 0, leftStart)) {
+            leftStart++;
+        }
+
+        var leftLength = 0;
+        while (readPixel(image, 0, leftStart + leftLength)) {
+            leftLength++;
+        }
+
+        var topStart = 0;
+        while (!readPixel(image, topStart, 0)) {
+            topStart++;
+        }
+
+        var topLength = 0;
+        while (readPixel(image, topStart + topLength, 0)) {
+            topLength++;
+        }
+
+        var outputDir = new File(file + "_cutted");
+        outputDir.mkdir();
+
+        writeFile(image, outputDir, "left_top.png", 1, 1, topStart - 1, leftStart - 1);
+
+        writeFile(image, outputDir, "center_top.png", topStart, 1, topLength, leftStart - 1);
+        writeFile(image, outputDir, "right_top.png", topStart + topLength, 1, image.getWidth() - topStart - topLength - 1, leftStart - 1);
+        writeFile(image, outputDir, "left_center.png", 1, leftStart, topStart - 1, leftLength);
+        writeFile(image, outputDir, "center_center.png", topStart, leftStart, topLength, leftLength);
+        writeFile(image, outputDir, "right_center.png", topStart + topLength, leftStart, image.getWidth() - topStart - topLength - 1, leftLength);
+        writeFile(image, outputDir, "left_bottom.png", 1, leftStart + leftLength, topStart - 1, image.getHeight() - leftStart - leftLength - 1);
+        writeFile(image, outputDir, "center_bottom.png", topStart, leftStart + leftLength, topLength, image.getHeight() - leftStart - leftLength - 1);
+        writeFile(image, outputDir, "right_bottom.png", topStart + topLength, leftStart + leftLength, image.getWidth() - topStart - topLength - 1, image.getHeight() - leftStart - leftLength - 1);
+    }
+
     public static void main(String[] args) throws IOException {
         for (var fileName : args) {
-            var file = new File(fileName);
-            var image = ImageIO.read(file);
-            if (image == null) {
-                System.out.println("Bad file: " + fileName);
-                continue;
-            }
-            System.out.println("Process image: " + fileName);
-
-            var leftStart = 0;
-            while (true) {
-                var color = image.getRGB(0, leftStart) & 0xFFFFFF;
-                if (readPixel(image, 0, leftStart)) {
-                    break;
-                }
-                leftStart++;
-            }
-
-            var leftLength = 0;
-            while (true) {
-                if (!readPixel(image, 0, leftStart + leftLength)) {
-                    break;
-                }
-                leftLength++;
-            }
-
-            var topStart = 0;
-            while (true) {
-                if (readPixel(image, topStart, 0)) {
-                    break;
-                }
-                topStart++;
-            }
-
-            var topLength = 0;
-            while (true) {
-                if (!readPixel(image, topStart + topLength, 0)) {
-                    break;
-                }
-                topLength++;
-            }
-
-            var outputDir = new File(fileName + "_cutted");
-            outputDir.mkdir();
-
-            var leftTop = image.getSubimage(1, 1, topStart - 1, leftStart - 1);
-            ImageIO.write(leftTop, "png", new File(outputDir, "left_top.png"));
-            var centerTop = image.getSubimage(topStart, 1, topLength, leftStart - 1);
-            ImageIO.write(centerTop, "png", new File(outputDir, "center_top.png"));
-            var rightTop = image.getSubimage(topStart + topLength, 1, image.getWidth() - topStart - topLength - 1, leftStart - 1);
-            ImageIO.write(rightTop, "png", new File(outputDir, "right_top.png"));
-            var leftCenter = image.getSubimage(1, leftStart, topStart - 1, leftLength);
-            ImageIO.write(leftCenter, "png", new File(outputDir, "left_center.png"));
-            var centerCenter = image.getSubimage(topStart, leftStart, topLength, leftLength);
-            ImageIO.write(centerCenter, "png", new File(outputDir, "center_center.png"));
-            var rightCenter = image.getSubimage(topStart + topLength, leftStart, image.getWidth() - topStart - topLength - 1, leftLength);
-            ImageIO.write(rightCenter, "png", new File(outputDir, "right_center.png"));
-            var leftBottom = image.getSubimage(1, leftStart + leftLength, topStart - 1, image.getHeight() - leftStart - leftLength - 1);
-            ImageIO.write(leftBottom, "png", new File(outputDir, "left_bottom.png"));
-            var centerBottom = image.getSubimage(topStart, leftStart + leftLength, topLength, image.getHeight() - leftStart - leftLength - 1);
-            ImageIO.write(centerBottom, "png", new File(outputDir, "center_bottom.png"));
-            var rightBottom = image.getSubimage(topStart + topLength, leftStart + leftLength, image.getWidth() - topStart - topLength - 1, image.getHeight() - leftStart - leftLength - 1);
-            ImageIO.write(rightBottom, "png", new File(outputDir, "right_bottom.png"));
+            cut(new File(fileName), Logger.getGlobal());
         }
     }
 }
